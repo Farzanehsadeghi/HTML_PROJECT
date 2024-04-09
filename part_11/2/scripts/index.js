6002c49fa161a54dad53433d8e4c50049ea4ff8b
Farzaneh  Sadeghi <158843212+Farzanehsadeghi@users.noreply.github.com>
2024-04-09 20:50:03 +0330

    Add files via upload

---
+++ part_11/2/scripts/index.js
@@ -0,0 +1,13 @@
+
+function light(x){
+        var pic;
+        if(x==0){
+            pic ="images/pic_bulboff.gif"
+            
+               
+            }
+         else{
+            pic ="images/pic_bulbon.gif"
+            }
+            document.getElementById('myImage').src = pic;
+        }
